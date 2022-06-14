7852e94eea2cab38fe239c6536849bff87ab64b8
dawitAmicho <dave.amicho@gmail.com>
2022-06-14 13:24:30 -0300

    not important but included

---
+++ src/database.js
@@ -0,0 +1,46 @@
+export  const database= [
+{
+id: 1,
+name: 'dave',
+email:'david.o@gmail.com'	
+},
+{
+id: 30,
+name: 'spak',
+email: 'sak.me@gmail.com'	
+},{
+id:10,
+name: 'tran',
+email: 'trans.atlanta@gmail.com'	
+},{
+id:170,
+name: 'good',
+email: 'badman.nogood@gmail.com'
+},
+{
+id: 130,
+name: 'ugly',
+email: 'fug.lee@gmail.com'	
+},{
+id:190,
+name: 'terrible',
+email: 'Ivan the terrible@gmail.com'	
+},{
+id:1170,
+name: 'fantastiko',
+email: 'fanta orange.nogood@gmail.com'	
+},
+{
+id: 230,
+name: 'sharky',
+email: 'shakry.me@gmail.com'	
+},{
+id:920,
+name: 'activist',
+email: 'acty.visty@gmail.com'	
+},{
+id:1720,
+name: 'irobot',
+email: 'Irobo.nogood@gmail.com'
+}
+]
