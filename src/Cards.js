cc711a91c11326f90430b78ae043e1100402c695
dawitAmicho <dave.amicho@gmail.com>
2022-06-21 23:25:26 -0300

    final edit

--- src/Cards.js
+++ src/Cards.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Cards = ({id, name, email}) =>
 {
-  return (
-<div id="cardAcc"  className='  dib br2 pa2 ma2 grow mw5 shadow-5 '  >
+  const rotation =()=>{
+    document.getElementById('cardAcc').style.color='yellow';
+  }
+  return ( 
+<div id="cardAcc" onClick={rotation}
+ className='  dib br2 pa2 ma2 grow mw5 shadow-5 '  >
 <img  alt='me' src = {`https://robohash.org/${id}?150*150`} />
+
+  
 <div>
 <h2>{ name } </h2>
 <p>{ email }</p>
